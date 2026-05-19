50d65721dbf664e55bab252fd44223181b6218ef
happyrilla <basukabomberumpe@gmail.com>
2026-05-19 11:52:56 +0200

    panic system

--- js/panic.js
+++ js/panic.js
@@ -1,5 +1,8 @@
-(function(){
+(function() {
   const b = localStorage.getItem("panicbutton") || "p";
   const l = localStorage.getItem("paniclink") || "https://www.google.com";
-  window.addEventListener("keydown", e => { if (e.key === b) window.location.href = l; });
+
+  window.addEventListener("keydown", e => {
+    if (e.key === b) window.location.href = l;
+  });
 })();
