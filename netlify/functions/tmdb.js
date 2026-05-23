7c9f1778bbfb52f6107e9e9ce9da64bd673d4cc7
happyrilla <basukabomberumpe@gmail.com>
2026-05-23 11:14:05 +0200

    plz

--- netlify/functions/tmdb.js
+++ netlify/functions/tmdb.js
@@ -1,8 +1,12 @@
-exports.handler = async function () {
+exports.handler = async function (event) {
   const API_KEY = process.env.TMDB_KEY;
+  const q = event.queryStringParameters.q || "matrix";
 
   const url =
-    "https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY;
+    "https://api.themoviedb.org/3/search/movie?api_key=" +
+    API_KEY +
+    "&query=" +
+    encodeURIComponent(q);
 
   const res = await fetch(url);
   const data = await res.json();
