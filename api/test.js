774120ab0947e96f371ab6032be3498ba07b90dd
happyrilla <basukabomberumpe@gmail.com>
2026-05-23 22:44:57 +0200

    test

--- api/test.js
+++ api/test.js
@@ -1,14 +1,2 @@
-module.exports = function(req, res) {
-  try {
-    // Hent variabelen din fra Vercel Dashboard
-    const dashboardVerdi = process.env.ello;
-    
-    // Hvis variabelen er tom, sender vi en hjelpetekst i stedet for å krasje
-    const svarVerdi = dashboardVerdi || "Fant ikke variabelen 'ello' i Vercel Dashboard";
-    
-    res.status(200).json({ verdi: svarVerdi });
-  } catch (error) {
-    res.status(500).json({ feilmelding: error.message });
-  }
-};
-console.log(svarVerdi)
+const something = Process.env.ello
+console.log(something)
